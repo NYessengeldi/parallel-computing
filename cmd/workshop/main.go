3a04232d7dc2a645a842265de6ef4d7336efca6f
Nurdaulet Y <esengeldynurdaulet@gmail.com>
2023-12-15 22:57:43 +0600

    add concurrency

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -22,19 +25,27 @@ func timer(name string) func() {
 func main() {
 	defer timer("main")()
 
+	var wg sync.WaitGroup
+
 	r, err := os.Open("input")
 	if err != nil {
 		panic(err)
 	}
 	defer r.Close()
 
-	result := newCounter()
-	s := bufio.NewScanner(r)
-	for s.Scan() {
-		for key := range result {
-			result[key] += strings.Count(s.Text(), key)
-		}
+	in := make(chan string)
+
+	go read(r, in)
+	counters := make([]map[string]int, runtime.NumCPU())
+	for i := 0; i < len(counters); i++ {
+		wg.Add(1)
+		counters[i] = newCounter()
+		go process(in, counters[i], &wg)
 	}
+
+	wg.Wait()
+
+	result := merge(counters)
 	overall := 0
 	for key, value := range result {
 		overall += value
@@ -42,4 +53,33 @@ func main() {
 	}
 	result["overall"] = overall
 	fmt.Printf("Overall: %d\n", result["overall"])
+}
+
+func merge(counters []map[string]int) map[string]int {
+	result := newCounter()
+	for _, counter := range counters {
+		for key, value := range counter {
+			result[key] += value
+		}
+	}
+	return result
+}
+
+func read(r io.Reader, out chan<- string) {
+	s := bufio.NewScanner(r)
+
+	for s.Scan() {
+		out <- s.Text()
+	}
+
+	close(out)
+}
+
+func process(in <-chan string, result map[string]int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for line := range in {
+		for key := range result {
+			result[key] += strings.Count(line, key)
+		}
+	}
 }
